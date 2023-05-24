c1ca16cfe76e176061cce5afd2b14e2e881210a4
Victor Roberto Curzio <victor.curzio@hotmail.com>
2023-05-24 19:52:47 -0300

    Modificación sobre la creación de las cartas

--- js/aventurero.js
+++ js/aventurero.js
@@ -9,52 +9,79 @@ function creacionAventurero(e) {
     );
 
     if (aventureroSeleccionado) {
-        let contenedor = document.createElement("div");
-        contenedor.className = "container mt-3 aventureroCard";
+        const contenedor = document.createElement("div");
+        contenedor.className = "mt-3 aventureroCard";
 
-        let row = document.createElement("div");
+        const row = document.createElement("div");
         row.className = "row";
 
-        let columna = document.createElement("div");
+        const columna = document.createElement("div");
         columna.className = "col";
 
-        let carta = document.createElement("div");
+        const carta = document.createElement("div");
         carta.className = "card";
-        carta.style = "width: 15vw";
 
-        carta.innerHTML = `<img class="card-img-top" src="${aventureroSeleccionado.img}" alt="${aventureroSeleccionado.nombre}" style="width:100%">
-                        <div class="card-body">
-                            <h4 class="card-title">${aventureroSeleccionado.nombre}</h4>
-                            <ul>
-                                <li class="card-text">Vida: ${aventureroSeleccionado.vida}</li>
-                                <li>Mana: ${aventureroSeleccionado.mana}</li>
-                                <li>Daño: ${aventureroSeleccionado.daño}</li>
-                                <li>Dinero: ${aventureroSeleccionado.dinero}</li>
-                            </ul>
-                        </div>`;
+        carta.innerHTML = `
+        <img class="card-img-top" src="${aventureroSeleccionado.img}" alt="${aventureroSeleccionado.nombre}" style="width:100%">
+        <div class="card-body">
+        <h4 class="card-title">${aventureroSeleccionado.nombre}</h4>
+        <ul>
+            <li class="card-text">Vida: ${aventureroSeleccionado.vida}</li>
+            <li>Mana: ${aventureroSeleccionado.mana}</li>
+            <li>Daño: ${aventureroSeleccionado.daño}</li>
+            <li>Dinero: ${aventureroSeleccionado.dinero}</li>
+        </ul>
+        </div>
+        <button type="button" class="btn btn-dark" id="btnAventureroTablero">Tablero</button>
+        <button type="button" class="btn btn-dark btnAventureroBorrar">Borrar</button>`;
 
         columna.appendChild(carta);
         row.appendChild(columna);
         contenedor.appendChild(row);
-        document.querySelector("main").appendChild(contenedor);
+        document.querySelector(".cartasAventurero").appendChild(contenedor);
         Swal.fire({
             icon: 'success',
-            title: 'Carta creada',
+            title: 'Creada',
+            text: 'Tu carta fue creada con éxito!',
             showConfirmButton: false,
             timer: 1000
-        })
+        });
+
+        const borrarAventurero = carta.querySelector(".btnAventureroBorrar");
+        borrarAventurero.addEventListener("click", () => eliminarAventurero(contenedor));
     }
 }
 
-const borrarAventurero = document.querySelector("#btnAventureroBorrar");
-borrarAventurero.addEventListener("click", eliminarAventurero);
-function eliminarAventurero(e) {
-    let borrar = document.querySelector(".aventureroCard");
-    borrar.remove();
-    Swal.fire({
+function eliminarAventurero(contenedor) {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    });
+
+    swalWithBootstrapButtons.fire({
+        title: '¿Estás seguro?',
+        text: 'Se eliminará la carta.',
         icon: 'warning',
-        title: 'Carta eliminada',
-        showConfirmButton: false,
-        timer: 1700
-    })
+        showCancelButton: true,
+        confirmButtonText: 'Sí, ¡borrar!',
+        cancelButtonText: 'No, cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            contenedor.remove();
+            swalWithBootstrapButtons.fire(
+                '¡Borrada!',
+                'Tu carta ha sido borrada con éxito.',
+                'success'
+            );
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            swalWithBootstrapButtons.fire(
+                'Cancelado',
+                'Tu carta se ha salvado.',
+                'error'
+            );
+        }
+    });
 }
